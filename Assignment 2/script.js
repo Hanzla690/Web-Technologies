8e4b1495880e31323fdef0162dfd2b02dce15548
Hanzla Khan <hanzlakhan690@gmail.com>
2024-03-17 20:58:19 +0500

    Added Form Validation and Responsiveness

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,16 +1,38 @@
+// Display Nav on Mobile
 let hamburger = document.getElementById("header-menu");
 let header = document.getElementById("header");
 hamburger.addEventListener("click", () => {
   header.classList.toggle("mobile");
 });
 
+// Code to handle form validation
 $("#Submit").click((event) => {
-  if (
-    $("#Name").val() == "" ||
-    $("#Email").val() == "" ||
-    $("#Message").val() == ""
-  ) {
-    alert("Empty String");
+  $("#Name, #Email, #Message").next(".error-text").remove();
+  if ($("#Name").val() == "") {
+    $("#Name").addClass("error");
+    $("<span class='error-text'>Please fill out this field</span>").insertAfter(
+      $("#Name")
+    );
     event.preventDefault();
   }
+  if ($("#Email").val() == "") {
+    $("#Email").addClass("error");
+    $("<span class='error-text'>Please fill out this field</span>").insertAfter(
+      $("#Email")
+    );
+    event.preventDefault();
+  }
+  if ($("#Message").val() == "") {
+    $("#Message").addClass("error");
+    $("<span class='error-text'>Please fill out this field</span>").insertAfter(
+      $("#Message")
+    );
+    event.preventDefault();
+  }
+});
+
+// Clear Error Message
+$("input, textarea").focus((event) => {
+  $(event.target).removeClass("error");
+  $(event.target).next(".error-text").remove();
 });
